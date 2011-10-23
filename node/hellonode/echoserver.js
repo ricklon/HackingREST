b7e269ef1352c39cbc4114af579bd2a51ca90081
Ricklon <rick.rickanderson@gmail.com>
2011-10-23 06:58:48 +0000

    added test url to thingspeak.html

--- node/hellonode/echoserver.js
+++ node/hellonode/echoserver.js
@@ -5,6 +5,7 @@ http.createServer(function (req, res) {
   echovalue = JSON.stringify(req.method) + " : " + JSON.stringify(url.parse(req.url)) + "\n";
   res.writeHead(200, {'Content-Type': 'text/plain',
   'Access-Control-Allow-Origin': '*'});
+  console.log(echovalue);
   res.end(echovalue);
 }).listen(process.env.C9_PORT, "0.0.0.0");
 console.log('Server running on port: ' + process.env.C9_PORT);
