3117c78122e8e540ce01db63f05019c2cdf7cb21
Ricklon <rick.rickanderson@gmail.com>
2011-10-23 03:50:57 -0400

    Fixed naming problem.

--- node/hellonode/http-proxy-onport.js
+++ node/hellonode/http-proxy-onport.js
@@ -1,14 +1,16 @@
 //node-http-proxy
 //From tutorial: http://blog.nodejitsu.com/http-proxy-intro
 
-var http = require('http'),
-    httpProxy = require('http-proxy');
+var http = require('http');
+var httpProxy = require('http-proxy');
+var PORT = 8000;
+
 //
 // Create a basic proxy server in one line of code...
 //
 // This listens on port 8000 for incoming HTTP requests 
 // and proxies them to port 9000
-httpProxy.createServer(80, 'api.thingspeak.com').listen( process.env.C9_PORT);
+httpProxy.createServer(80, 'api.thingspeak.com').listen(PORT);
 
 //
 // ...and a simple http server to show us our request back.
@@ -17,5 +19,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
   res.end();
-}).listen(process.env.C9_PORT, "0.0.0.0");
-console.log('Server running on port: ' + process.env.C9_PORT);
+}).listen(PORT, "127.0.0.1");
+console.log('Server running on port: ' + PORT);
