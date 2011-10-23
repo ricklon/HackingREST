1e406e7f7712549aea06bff94769dde2a0c1a635
Ricklon <rick.rickanderson@gmail.com>
2011-10-23 02:58:12 -0400

    changes made to postexample.

--- node/hellonode/postexample.js
+++ node/hellonode/postexample.js
@@ -1,17 +1,24 @@
 //Node example of client post
 //Modified from: http://stackoverflow.com/questions/4579757/how-do-i-create-a-http-client-request-with-a-cookie
 var http = require('http');
+var sys = require('sys');
+var util = require('util');
+
 var key = "ZGS9MSQYCROJUWK6";
 var xx = 11;
 var yy = 22;
 var zz = 33;
+//var domain = "hackingrest.ricklon.cloud9ide.com";
+var PORT = 80;
+var domain = "api.thingspeak.com";
+//var PORT = 80;
 
 var data = JSON.stringify({"key": key, "field1": xx, "field2": yy, "field3": zz });
 
-var client = http.createClient(80, 'api.thingspeak.com');
+var client = http.createClient(PORT, domain);
 
 var headers = {
-    'Host': 'api.thingspeak.com',
+    'Host': domain,
     'Content-Type': 'application/json',
     'Content-Length': data.length
 };
@@ -20,15 +27,25 @@ var request = client.request('POST', '/update', headers);
 
 // listening to the response is optional, I suppose
 request.on('response', function(response) {
+	//sys.debug( JSON.stringify(response));
+	sys.debug(util.inspect(response, true, null));
+	//sys.debug(util.inspect(response, true));
   response.on('data', function(chunk) {
-    // do what you do
+	console.log(data);
   });
   response.on('end', function() {
     // do what you do
+
   });
 });
 // you'd also want to listen for errors in production
+request.on('error', function(err) {
+        sys.debug('unable to connect to ' + url);
+    });
 
 request.write(data);
 
 request.end();
+
+console.log("Completed");
+
